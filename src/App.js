6b8da073899b52b22a0aa120658ef3a60ab80753
rtezen23 <raul_tezen@hotmail.com>
2022-06-22 17:45:40 -0500

    entregable01 checked

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
 import './App.css';
 import Author from './components/Author';
 import Quote from './components/Quote';
-import quotes from './quotes.json';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
-
-const colors = ['#7FB5FF', '#006E7F', '#B22727', '#AB46D2', '#446A46', '#55D8C1', '#FF6FB5', '#532E1C',
-'#1EE494', '#616EEF', '#064ACB', '#481380', '#8BFFFF', '#FFBD35', '#26BAEE', '#081F37', '#D5441C', '#28FFBF'];
+const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
 
 function App() {
 
-  let firstRandom = 0 + Math.floor(Math.random()*quotes.length);
-  const [random, setRandom] = useState(firstRandom);
+  //Set quotes from the API
+  const [quotes, setQuotes] = useState([]);
+  
+  useEffect(()=>{
+    fetch('https://jsonblob.com/api/jsonblob/918261686465740800')
+    .then(res=>res.json())
+    .then(data=> setQuotes(data));
+  },[]);
 
-  let firstColor = 0 + Math.floor(Math.random()*colors.length);
-  const [color, setColor] = useState(colors[firstColor]);
-  document.body.style = `background: ${color}`;
+  //Function to Get a random hex color => ex: it could return '#123abc'
+  const newColor = () => {
+    let updatedColor = '#';
+    for (let i = 0; i < 6; i++) {
+      const aleatorio = 0 + Math.floor(Math.random()*hex.length);
+      updatedColor+=hex[aleatorio];
+    } 
+    return updatedColor;
+  }
 
+  const [randomColor, setRandomColor] = useState(newColor());
+
+  //Function to get a random number (that will be used as index) of our quotes json
+  const randomQuote = () => 0 + Math.floor(Math.random()*quotes.length);
+
+  //Set a random number to the state
+  const [random, setRandom] = useState(randomQuote());
+
+  //set the body random background
+  document.body.style = `background: ${randomColor}`;
+
+  //function to get a new random index and a new color. That will re-render the component so we'll see a new color in our page
   const changeAuthor = () => {
-    firstRandom = 0 + Math.floor(Math.random()*quotes.length);
-    setRandom(firstRandom);
-    firstColor = 0 + Math.floor(Math.random()*colors.length);
-    setColor(colors[firstColor]);
-    document.body.style = `background: ${color}`;
+    setRandom(randomQuote);
+    setRandomColor(newColor());
   };
-
+  
   return (
     <div className="App">
-      <Quote index={random} color={color} />
-      <Author index={random} color={color} action={changeAuthor}/>
+      <Quote index={random} color={randomColor} />
+      <Author index={random} color={randomColor} action={changeAuthor}/>
     </div>
   );
 };
